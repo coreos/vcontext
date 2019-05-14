517a9142fd03d0938eaa4774bdc15a8940350a86
Andrew Jeddeloh <andrew.jeddeloh@coreos.com>
2019-05-14 13:38:36 -0700

    tree: move to own package
    Additionally add some more helpers in report.

--- json/json.go
+++ json/json.go
@@ -1,12 +1,12 @@
 package json
 
 import (
-	"github.com/ajeddeloh/vcontext"
+	"github.com/ajeddeloh/vcontext/tree"
 	// todo: rewrite this dep
 	json "github.com/ajeddeloh/go-json"
 )
 
-func UnmarshalToContext(raw []byte) (vcontext.Node, error) {
+func UnmarshalToContext(raw []byte) (tree.Node, error) {
 	var ast json.Node
 	if err := json.Unmarshal(raw, &ast); err != nil {
 		return nil, err
@@ -14,34 +14,34 @@ func UnmarshalToContext(raw []byte) (vcontext.Node, error) {
 	return fromJsonNode(ast), nil
 }
 
-func fromJsonNode(n json.Node) vcontext.Node {
-	m := vcontext.MarkerFromIndices(int64(n.Start), int64(n.End))
+func fromJsonNode(n json.Node) tree.Node {
+	m := tree.MarkerFromIndices(int64(n.Start), int64(n.End))
 
 	switch v := n.Value.(type) {
 	case map[string]json.Node:
-		ret := vcontext.MapNode{
+		ret := tree.MapNode{
 			Marker: m,
-			Children: make(map[string]vcontext.Node, len(v)),
-			Keys:     make(map[string]vcontext.Leaf, len(v)),
+			Children: make(map[string]tree.Node, len(v)),
+			Keys:     make(map[string]tree.Leaf, len(v)),
 		}
 		for key, child := range v {
 			ret.Children[key] = fromJsonNode(child)
-			ret.Keys[key] = vcontext.Leaf{
-				Marker: vcontext.MarkerFromIndices(int64(child.KeyStart), int64(child.KeyEnd)),
+			ret.Keys[key] = tree.Leaf{
+				Marker: tree.MarkerFromIndices(int64(child.KeyStart), int64(child.KeyEnd)),
 			}
 		}
 		return ret
 	case []json.Node:
-		ret := vcontext.SliceNode{
+		ret := tree.SliceNode{
 			Marker: m,
-			Children: make([]vcontext.Node, 0, len(v)),
+			Children: make([]tree.Node, 0, len(v)),
 		}
 		for _, child := range v {
 			ret.Children = append(ret.Children, fromJsonNode(child))
 		}
 		return ret
 	default:
-		return vcontext.Leaf{
+		return tree.Leaf{
 			Marker: m,
 		}
 	}
